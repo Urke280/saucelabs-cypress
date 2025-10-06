a6d58ad53d05013ad955923dab785c623f50191d
Urke280 <uros@oldtajmeri.rs>
2025-10-06 11:10:29 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 reporter: 'cypress-mochawesome-reporter',
+reporterOptions: {
+  reportDir: "cypress/reports",
+  overwrite: false,
+  html: false,
+  json: true
+},
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
+    supportFile: "cypress/support/e2e.js",
   chromeWebSecurity: false,
   experimentalStudio: true,
   },
